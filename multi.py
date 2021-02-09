b4a73a155a60b945caf4634b05997d97c573020e
Marinto-Richee <marinto3sja@gmail.com>
2021-02-09 05:43:16 +0000

    First Version

--- multi.py
+++ multi.py
@@ -1,22 +1,3 @@
-# Developing a Simple Webserver
-## AIM:
-To develop a simple webserver to serve html pages.
-
-## DESIGN STEPS:
-### Step 1: 
-HTML content creation
-### Step 2:
-Design of webserver workflow
-### Step 3:
-Implementation using Python code
-### Step 4:
-Serving the HTML pages.
-### Step 5:
-Testing the webserver
-
-## PROGRAM:
-### MULTI.PY:
-```
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
 content="""
@@ -52,11 +33,3 @@ class Mypage(BaseHTTPRequestHandler):
 server_address=('',80)
 httpd = HTTPServer(server_address,Mypage)
 httpd.serve_forever()
-```
-
-## OUTPUT:
-![output](./multi.png)
-
-## RESULT:
-Thus a website is designed for Multiplcation Table
-URL http://marinto.student.saveetha.in. 
